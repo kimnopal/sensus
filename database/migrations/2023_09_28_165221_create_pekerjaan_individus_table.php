13f0066642b56b1a9ce6f13477063bf9a3d66072
kimnopal <naufalhakim366@gmail.com>
2023-09-29 00:08:47 +0700

    add pekerjaan individu migration

---
+++ database/migrations/2023_09_28_165221_create_pekerjaan_individus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pekerjaan_individu', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("individu_id");
+            $table->foreign("individu_id")->references("id")->on("individu");
+            $table->unsignedBigInteger("kondisi_pekerjaan_id");
+            $table->foreign("kondisi_pekerjaan_id")->references("id")->on("kondisi_pekerjaan");
+            $table->enum("status_jamsostek", ["peserta", "bukan peserta"]);
+            $table->string("no_jamsostek", 100)->nullable();
+            $table->unsignedBigInteger("gaji");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pekerjaan_individus');
+    }
+};
---
+++ database/migrations/2023_09_28_170022_create_penghasilan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penghasilan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("pekerjaan_individu_id");
+            $table->foreign("pekerjaan_individu_id")->references("id")->on("pekerjaan_individu");
+            $table->unsignedBigInteger("sumber_penghasilan_id");
+            $table->foreign("sumber_penghasilan_id")->references("id")->on("sumber_penghasilan");
+            $table->unsignedInteger("jumlah");
+            $table->unsignedBigInteger("satuan_id");
+            $table->foreign("satuan_id")->references("id")->on("satuan");
+            $table->unsignedInteger("penghasilan");
+            $table->enum("ekspor", ["semua", "sebagian besar", "tidak"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penghasilan');
+    }
+};
